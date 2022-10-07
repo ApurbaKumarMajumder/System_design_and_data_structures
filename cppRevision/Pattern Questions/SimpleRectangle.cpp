c8c6d02368cf3f4edffdd59e9670b2f7a0892cd5
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-10-07 23:22:58 +0530

    SImple rectangle question done

---
+++ cppRevision/Pattern Questions/SimpleRectangle.cpp
@@ -0,0 +1,28 @@
+// *****
+// *****
+// *****
+// *****
+// *****
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows, cols;
+    cout << "Enter the number of rows: \n";
+    cin >> rows;
+    cout << "Enter the number of columns: \n";
+    cin >> cols;
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= cols; j++)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
