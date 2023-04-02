690e271767592f7f9a6007c02fe4ee561607111a
ApurbaKumarMajumder <9851apurba@gmail.com>
2023-04-02 21:12:36 +0530

    guesser class

--- GameProject/LaunchGame.java
+++ GameProject/LaunchGame.java
@@ -1,4 +1,18 @@
 package GameProject;
+import java.util.*;
+
+class Guesser
+{
+    int guessNum;
+
+    int guessingNumber()
+    {
+        System.out.println("Guesser! Kindly guess the number");
+        Scanner scan = new Scanner(System.in);
+        guessNum = scan.nextInt();
+        return guessNum;
+    }
+}
 
 public class LaunchGame {
     public static void main(String[] args) {
