42bc31ff99d12989b3c112561bf565722a9d539d
ApurbaKumarMajumder <9851apurba@gmail.com>
2023-04-02 22:16:09 +0530

    working with umpire class

--- GameProject/LaunchGame.java
+++ GameProject/LaunchGame.java
@@ -14,6 +14,59 @@ class Guesser
     }
 }
 
+class Player 
+{
+    int guessNum;
+    int guessingNumber()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Player! Kindly guess the number");
+        guessNum = scan.nextInt();
+        return guessNum;
+    }
+}
+
+class Umpire
+{
+    int numFromGuesser;
+    int numFromPlayer1;
+    int numFromPlayer2;
+    int numFromPlayer3;
+
+    void collectNumFromGuesser() {
+        Guesser g = new Guesser();
+        numFromGuesser = g.guessingNumber();
+    }
+
+    void collectNumFromPlayer()
+    {
+        Player p1 = new Player();
+        Player p2 = new Player();
+        Player p3 = new Player();
+
+        numFromPlayer1 = p1.guessingNumber();
+        numFromPlayer2 = p2.guessingNumber();
+        numFromPlayer3 = p3.guessingNumber();
+    }
+
+    void compare()
+    {
+        if (numFromGuesser == numFromPlayer1) {
+            System.out.println("Player1 won the game");
+        }
+        else if (numFromGuesser == numFromPlayer2) {
+            System.out.println("Player2 won the game");
+        }
+        else if (numFromGuesser == numFromPlayer3) {
+            System.out.println("Player3 won the game");
+        }
+
+        else {
+            System.out.println("Sab ka ghalat hain. App game hargaye! ");
+        }
+    }
+}
+
 public class LaunchGame {
     public static void main(String[] args) {
         
